70baebeff5d9b84fe4dd02c800dc5351c5ca5869
BradBettig <brad.bettig9@gmail.com>
2023-03-10 20:18:48 -0800

    created neural net class

---
+++ neural-net-struct.js
@@ -0,0 +1,27 @@
+class neuralNet{
+	constructor(net_struct, inputs) {
+
+		this.net_struct = net_struct;
+		this.inputs = inputs;
+		this.layers = [];
+
+		const layerSizes = net_struct.split(",").map(size => parseInt(size.trim()));
+		
+		for(let i = 0; i < layerSizes.length; i++){
+			this.layers.push(new Array(layerSizes[i]));
+		}
+
+
+		
+		
+	}
+
+	// Methods go here
+}
+
+function main(){
+	let myObject = new neuralNet("3,2,1", "1");
+	console.log(myObject)
+}
+
+main();
