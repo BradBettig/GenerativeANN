5f63362d5f2a08a9d165230ec7251d878948edf3
BradBettig <brad.bettig9@gmail.com>
2023-03-10 20:51:44 -0800

    added node class

--- neural-net-struct.js
+++ neural-net-struct.js
@@ -1,21 +1,34 @@
 class neuralNet{
-	constructor(net_struct, inputs) {
+	constructor(layers, inputs) {
 
-		this.net_struct = net_struct;
+		this.layers = layers;
 		this.inputs = inputs;
-		this.layers = [];
+		this.net_struct = [];
 
-		const layerSizes = net_struct.split(",").map(size => parseInt(size.trim()));
+		const layerSizes = layers.split(",").map(size => parseInt(size.trim()));
 		
 		for(let i = 0; i < layerSizes.length; i++){
-			this.layers.push(new Array(layerSizes[i]));
+			let nodes = [];
+			this.net_struct.push(nodes = Array(layerSizes[i]));
+			for (let j = 0; j < nodes.length; j++)
+			{
+				nodes[j] = new node();
+			}
+
 		}	
 	}
 }
 
+class node{
+	constructor(){
+		this.collector = 0.0;
+		this.collections = [];
+	}
+}
 function main(){
 	let myObject = new neuralNet("3,2,1", "1");
-	console.log(myObject)
+	let jsonString = JSON.stringify(myObject);
+	console.log(jsonString);
 }
 
 main();
