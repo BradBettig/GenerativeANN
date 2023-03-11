df349efd59658aa56c8628bcdf5d1d687ac318aa
BradBettig <brad.bettig9@gmail.com>
2023-03-10 20:56:26 -0800

    added comments

--- neural-net-struct.js
+++ neural-net-struct.js
@@ -4,12 +4,14 @@ class neuralNet{
 		this.layers = layers;
 		this.inputs = inputs;
 		this.net_struct = [];
-
+		// create an array of integers
 		const layerSizes = layers.split(",").map(size => parseInt(size.trim()));
 		
 		for(let i = 0; i < layerSizes.length; i++){
 			let nodes = [];
+			// add layer to the neural net 
 			this.net_struct.push(nodes = Array(layerSizes[i]));
+			// add nodes to layer
 			for (let j = 0; j < nodes.length; j++)
 			{
 				nodes[j] = new node();
